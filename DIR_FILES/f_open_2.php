928256e66adfa468814dcc44bbd9a4c5d8cd4466
Ricardo Mazoni <ricardomazoni@gmail.com>
2017-12-08 18:05:13 -0200

    Manipulando arquivos fOpen

---
+++ DIR_FILES/f_open_2.php
@@ -0,0 +1,42 @@
+<?php
+
+
+require_once("config.php");
+
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+
+$headers =array();
+
+foreach ($usuarios[0] as $key => $value) {
+	array_push($headers, ucfirst($key));
+
+
+}
+
+$file = fopen("usuarios.csv", "w+");
+
+fwrite($file,implode(",", $headers)."\r\n");
+
+print_r($headers);
+
+
+foreach ($usuarios as $row) {
+	$data = array();
+	print_r($row);	
+
+	foreach ($row as $key => $value) {
+
+		array_push($data, $value);
+	}//End foreach de coluna
+
+	fwrite($file,implode(",", $data)."\r\n");
+
+}//End foreach de linha
+
+fclose($file);
+
+
+ ?>
