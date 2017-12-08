7b2350df030848fb1bb16acd8f81cd531c8cde63
Ricardo Mazoni <ricardomazoni@gmail.com>
2017-12-08 17:46:59 -0200

    criando funcoes no f_open.php

--- DIR_FILES/f_open.php
+++ DIR_FILES/f_open.php
@@ -1,4 +1,15 @@
-<?php 
+<?php
+
+//w+ é para escrever write. w+ escreve somente uma vez
+//a+ fica adicionando 
+$file = fopen("log.txt", "a+");
+
+fwrite($file, date("Y-m-d H:i:s")."\r\n");
+
+fclose($file);
+
+echo "Arquivo criado com sucesso!!";
+
 
 
 
