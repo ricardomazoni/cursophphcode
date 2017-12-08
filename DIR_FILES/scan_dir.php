be39657c24cda9eaa1790225904f2b3968b9fbc0
Ricardo Mazoni <ricardomazoni@gmail.com>
2017-12-08 17:25:18 -0200

    Manipulação de pastas e diretorios

--- DIR_FILES/scan_dir.php
+++ DIR_FILES/scan_dir.php
@@ -15,6 +15,20 @@ foreach ($images as $img) {
 
 		$info = pathinfo($filename);
 
+		//pega o nome do arquivo em bytes
+
+		$info["size"] = filesize($filename);
+
+		//ultima modificação do arquivo
+		$info["modified"] = date("d/m/Y H:i:s",filemtime($filename));
+
+		//acessar a imagem informada
+
+		$info["url"] = "http://localhost/CursoPHPCompleto/DIR_FILES/".str_replace("\\", "/", $filename);
+
+
+
+
 		//var_dump($info);
 		array_push($data, $info);
 	}
