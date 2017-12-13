fe06234dbf060f327f06deb4a957afb7fc03d6ad
Ricardo Mazoni <ricardomazoni@gmail.com>
2017-12-13 19:30:56 -0200

    Manipulando arquivos e pastas

--- DIR_FILES/mover_diretorios/mover_diretorios.php
+++ DIR_FILES/mover_diretorios/mover_diretorios.php
@@ -1,4 +1,22 @@
 <?php
 
+$dir1 = "folder_01";
+$dir2 = "folder_02";
 
+if(!is_dir($dir1)) mkdir($dir1);
+if(!is_dir($dir2)) mkdir($dir2);
+
+$file_name = "README.txt";
+
+if(!file_exists($dir1).DIRECTORY_SEPARATOR .$file_name){
+  $file = fopen($dir1.DIRECTORY_SEPARATOR .$file_name,"w+");
+  fwrite($file,date("Y-m-d H:i:s"));
+  fclose($file);
+}
+
+rename($dir1.DIRECTORY_SEPARATOR.$file_name,
+       $dir2.DIRECTORY_SEPARATOR.$file_name);
+
+echo "arquivo enviado de $dir1.DIRECTORY_SEPARATOR.$file_name ";
+echo "<br/>para $dir2.DIRECTORY_SEPARATOR.$file_name ";
  ?>
