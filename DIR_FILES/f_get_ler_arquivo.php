06ece3b374106b7325a27090e3be518903165fd8
Ricardo Mazoni <ricardomazoni@gmail.com>
2017-12-13 16:11:07 -0200

    Manipulando arquivos leitura de arquivo CSV

--- DIR_FILES/f_get_ler_arquivo.php
+++ DIR_FILES/f_get_ler_arquivo.php
@@ -7,17 +7,26 @@ if(file_exists($filename)){
 
   //fgtest - Pega o cabeçalho
   //explode retirar as virgulas...e quebra cada campo num indice do array
-
+  // Ao ler o arqui é posicionado para a proxima linha
   $headers = explode(",",fgets($file));
 
   $data = array();
 
   while ($row = fgets($file)) {
     $rowData = explode(",",$row);
+    $linha = array();
 
+    for ($i=0; $i < count($headers); $i++) {
+      $linha[$headers[$i]] = $rowData[$i];
+
+    }
+    array_push($data,$linha);
 
   }
   fclose($file);
+
+  echo json_encode($data);
+
 
    //var_dump($headers);
 
